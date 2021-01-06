4a8e95b37e64df5c2e02989782b234cbd34402f3
Kai Yuan Neo <kai@rocketacademy.co>
2021-01-06 23:33:23 +0800

    lint and update repo with sequelize underscore convention

--- controllers/items.mjs
+++ controllers/items.mjs
@@ -1,15 +1,14 @@
-export default function items(db){
-
-  const index = (request, response) => {
-
-    db.Item.findAll()
-    .then((items) => {
-      response.send({items});
-    })
-    .catch((error) => console.log(error));
+export default function initItemsController(db) {
+  const index = async (request, response) => {
+    try {
+      const items = await db.Item.findAll();
+      response.send({ items });
+    } catch (error) {
+      console.log(error);
+    }
   };
 
   return {
-    index
+    index,
   };
 }
